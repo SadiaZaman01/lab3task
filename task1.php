fde73de6408520e80549bf477c1db7694a559051
Sadia Zaman <sadia.sorno.zaman@gmail.com>
2021-02-08 16:06:50 +0600

    labtask done

---
+++ task1.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head> 
+<title> task-01 </title>
+</head>
+<body>
+
+A quick brown <?php echo "<b> fox </b>"; ?> jums over the lazy <?php echo "<b> dog </b>"; ?>
+
+
+</body>
---
+++ task3.php
@@ -0,0 +1,16 @@
+<?php 
+$length = 10;
+$width = 6;
+if($length ==$width) 
+{
+	echo " This shape is a square.";
+}
+else 
+{
+	$area = $length * $width ;
+	echo "Area is :". $area;
+	$perimeter = 2 * ($length + $width);
+	echo "<br> perimeter is : ".$perimeter;
+}
+?>
+	
